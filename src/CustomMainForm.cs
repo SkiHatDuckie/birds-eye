4570baf540886be467c4a2de88c02aedc9bb2ab7
ConeRobo <skihatduckie@gmail.com>
2021-12-31 17:54:07 -0500

    Handle invalid hexadecimal addresses

--- src/CustomMainForm.cs
+++ src/CustomMainForm.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 using BizHawk.Client.Common;
@@ -10,21 +11,21 @@ namespace BirdsEye {
     [ExternalTool("BirdsEye")]
     public partial class CustomMainForm : ToolFormBase, IExternalToolForm {
         /// <remarks>
-		/// <see cref="ApiContainer"/> can be used as a shorthand for accessing the various APIs, more like 
-        /// the Lua syntax.
+		/// <see cref="ApiContainer"/> can be used as a shorthand for accessing 
+        /// the various APIs, more like the Lua syntax.
 		/// </remarks>
 		public ApiContainer? _apiContainer { get; set; }
 
 		private ApiContainer APIs => _apiContainer ?? throw new NullReferenceException();
 
-        private Label lblRomName;
-        private Label lblMemory;
-        private ListBox lstAddress;
-        private ListBox lstMemory;
-        private Button btnNewAddress;
-        private TextBox txtAddress;
+        private Label _lblRomName;
+        private Label _lblMemory;
+        private ListBox _lstAddress;
+        private ListBox _lstMemory;
+        private Button _btnNewAddress;
+        private TextBox _txtAddress;
 
-        private List<long> memoryAddresses = new List<long>();
+        private List<long> _memoryAddresses = new List<long>();
 
         protected override string WindowTitleStatic => "BirdsEye";
 
@@ -36,52 +37,53 @@ namespace BirdsEye {
             ClientSize = new Size(320, 320);
             SuspendLayout();
 
-            lblRomName = new Label {
+            _lblRomName = new Label {
                 AutoSize = true,
                 Location = new Point(0, 0),
             };
-            lblMemory = new Label {
+            _lblMemory = new Label {
                 AutoSize = true,
                 Location = new Point(0, 25),
                 Text = "Memory"
             };
-            btnNewAddress = new Button {
+            _btnNewAddress = new Button {
                 Location = new Point(0, 45),
                 Size = new Size(100, 25),
                 Text = "Add Address:"
             };
-            txtAddress = new TextBox {
+            _txtAddress = new TextBox {
                 Location = new Point(100, 45),
 			    Size = new Size(70, 20)
             };
-            lstAddress = new ListBox {
+            _lstAddress = new ListBox {
                 Location = new Point(100, 70),
                 Size = new Size(70, 150)
             };
-            lstMemory = new ListBox {
+            _lstMemory = new ListBox {
                 Location = new Point(0, 70),
                 Size = new Size(100, 150)
             };
 
-            Controls.Add(lblRomName);
-            Controls.Add(lblMemory);
-            Controls.Add(btnNewAddress);
-            Controls.Add(txtAddress);
-            Controls.Add(lstAddress);
-            Controls.Add(lstMemory);
+            Controls.Add(_lblRomName);
+            Controls.Add(_lblMemory);
+            Controls.Add(_btnNewAddress);
+            Controls.Add(_txtAddress);
+            Controls.Add(_lstAddress);
+            Controls.Add(_lstMemory);
             ResumeLayout();
 
-            btnNewAddress.Click += btnNewAddress_Click;
+            _btnNewAddress.Click += btnNewAddress_Click;
         }
 
         ///<summary>
-        /// Executed once after the constructor, and again every time a rom is loaded or reloaded.
+        /// Executed once after the constructor, and again every time a rom is
+        /// loaded or reloaded.
         ///</summary>
         public override void Restart() {
             if (APIs.GameInfo.GetRomName() != "Null") {
-                lblRomName.Text = $"Currently loaded: {APIs.GameInfo.GetRomName()}";
+                _lblRomName.Text = $"Currently loaded: {APIs.GameInfo.GetRomName()}";
             } else {
-                lblRomName.Text = "Currently loaded: Nothing";
+                _lblRomName.Text = "Currently loaded: Nothing";
             }
         }
 
@@ -98,28 +100,42 @@ namespace BirdsEye {
         }
 
         ///<summary>
+        /// Determine if all characters in s are valid hexadecimal digits.
+        /// Returns false if an invalid digit is found, otherwise this returns true.
+        ///</summary>
+        private bool IsHexadecimal(string s) {
+            string hexadecimalChars = "0987654321ABCDEFabcdef";
+            foreach (char ch in s) {
+                if (!hexadecimalChars.Contains(ch)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        ///<summary>
         /// Executed after every frame.
         ///</summary>
         protected override void UpdateAfter() {
             if (APIs.GameInfo.GetRomName() != "Null") {
-                uint[] memoryData = ReadMemory(memoryAddresses.ToArray());
-                for (int i = 0; i < lstMemory.Items.Count; i++) {
-                    lstMemory.Items[i] = memoryData[i].ToString();
+                uint[] memoryData = ReadMemory(_memoryAddresses.ToArray());
+                for (int i = 0; i < _lstMemory.Items.Count; i++) {
+                    _lstMemory.Items[i] = memoryData[i].ToString();
                 }
             }
         }
 
         ///<summary>
-        /// Update the `lstAddress`, `lstMemory`, and `memoryAddresses` with the entered address in
-        /// `txtAddress`. 
-        ///
-        /// TODO: Handle invalid addresses or addresses that don't exist
+        /// Update the lstAddress, lstMemory, and memoryAddresses with the entered 
+        /// address in txtAddress.
         ///</summary>
         private void btnNewAddress_Click(object sender, EventArgs e) {
-            if (!string.IsNullOrWhiteSpace(txtAddress.Text) && !lstAddress.Items.Contains(txtAddress.Text)) {
-                lstAddress.Items.Add(txtAddress.Text);
-                lstMemory.Items.Add("-");
-                memoryAddresses.Add(Convert.ToInt64(txtAddress.Text, 16));
+            if (!string.IsNullOrWhiteSpace(_txtAddress.Text) && !_lstAddress.Items.Contains(_txtAddress.Text)) {
+                if (IsHexadecimal(_txtAddress.Text)) {
+                    _memoryAddresses.Add(Convert.ToInt64(_txtAddress.Text, 16));
+                    _lstAddress.Items.Add(_txtAddress.Text);
+                    _lstMemory.Items.Add("-");
+                }
             }
         }
     }
