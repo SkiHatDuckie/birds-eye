c2c955f2bedf2fbcd424b3a342e979dd92dae680
SkiHatDuckie <SkiHatDuckie@gmail.com>
2024-12-17 20:29:17 -0500

    Implement `Request` for controller input

--- birdseyelib/controllerInput.py
+++ birdseyelib/controllerInput.py
@@ -1,34 +1,42 @@
-class ControllerInput:
-    """Class containing various functions for reading memory from BizHawk."""
-    def __init__(self, client):
-        self.client = client
+from request import Request
 
-    def set_joypad(self, joypad):
-        """Sets the joypad layout to be used when setting inputs.
-        
-        Layout is set to the NES joypad by default.
 
-        :param joypad: The joypad layout to send to the external tool. \
-        Make sure the joypad matches the emulated system being used!
-        :type joypad: Joypad"""
-        self.client._queue_request("INP_JOYPAD;" + joypad._name + "\n")
+class JoypadConfig(Request):
+    """Sets the joypad layout to be used when setting inputs.
 
-    def set_controller_input(self, joypad):
-        """Sets the controller inputs to be executed in the emulator.
-        All inputs are set to `False` be default.
-        The inputs are executed until a new controller input is sent.
+    Layout is set to the NES joypad by default.
+    
+    :param joypad: The joypad layout to send to the external tool. \
+    Make sure the joypad matches the emulated system being used!
+    :type joypad: Joypad"""
+    def __init__(self, client, joypad):
+        super().__init__("INP_JOYPAD", client)
+        self.joypad = joypad
 
-        :param joypad: The joypad layout to send to the external tool. \
-        Make sure the joypad matches the emulated system being used!
-        :type joypad: Joypad"""
-        bool_to_string = {False : "false", True : "true"}
-        controller_input = ";".join(
-            [bool_to_string[joypad.controls[button]] for button in joypad.controls.keys()]
-        )
-        self.client._queue_request("INP_SET;" + controller_input + "\n")
-        if hasattr(joypad, "analog_controls"):
-            controller_analog_input = ";".join(
-                [str(joypad.analog_controls[analog_control])
-                 for analog_control in joypad.analog_controls.keys()]
-            )
-            self.client._queue_request("INP_SET_ANALOG;" + controller_analog_input + "\n")
+    def queue(self):
+        self.client._queue_request(self.tag + ";" + self.joypad + "\n")
+
+class ControllerInputs(Request):
+    """Sets the controller inputs to be executed in the emulator.
+    All inputs are set to `False` be default.
+    The inputs are executed until a new controller input is sent.
+    
+    :param joypad: The joypad layout to send to the external tool. \
+    Make sure the joypad matches the emulated system being used!
+    :type joypad: Joypad"""
+    def __init__(self, client, joypad):
+        super().__init__("INP_SET", client)
+        self.joypad = joypad
+
+    def queue(self):
+        controller_input = [
+            str(self.joypad.controls[button]).lower()
+            for button in self.joypad.controls.keys()
+        ]
+        if hasattr(self.joypad, "analog_controls"):
+            controller_input += [
+                str(self.joypad.analog_controls[analog_control])
+                for analog_control in self.joypad.analog_controls.keys()
+            ]
+
+        self.client._queue_request(self.tag + ";" + controller_input + "\n")
